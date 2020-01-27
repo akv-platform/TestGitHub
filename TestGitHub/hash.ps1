4cef7ba3a0c035dd990f3c88e3f6e31969688c59
Sergey Dolin <sergey.dolin@gmail.com>
2020-01-27 13:03:24 +0500

    Update hash.ps1

--- TestGitHub/hash.ps1
+++ TestGitHub/hash.ps1
@@ -1,6 +1,2 @@
-[environment]::Version
-$env:PSModulePath
 $env:PSModulePath = "$PSHOME/Modules"
-[environment]::Version
-$env:PSModulePath
 Get-FileHash -Path "D:\a\TestGitHub\TestGitHub\TestGitHub\hash.ps1"
